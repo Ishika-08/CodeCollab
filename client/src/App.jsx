800d7667be3aef30e2bcc84143d7376146fd7f58
Himanshu <himanshusuryawanshi45@gmail.com>
2024-03-16 14:20:11 +0530

    Create login, Register & Reset-password pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div>
-      <h1 className="bg-red-500">App</h1>
-    </div>
-  )
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import ResetPassword from "./pages/resetPassword";
 
-export default App
+ export default function App() {
+   return (
+     <Router>
+       <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+       </Routes>
+     </Router>
+
+   );
+  }
+

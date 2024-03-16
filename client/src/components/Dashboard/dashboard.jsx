6989624897e4b1bc0083d59e6f23c534829b3ae6
Himanshu <himanshusuryawanshi45@gmail.com>
2024-03-17 01:13:04 +0530

    new card added in dashboard & changes done in features section

--- client/src/components/Dashboard/dashboard.jsx
+++ client/src/components/Dashboard/dashboard.jsx
@@ -15,7 +15,7 @@ export default function Dashboard() {
 
                 <div className="mt-[10%] ml-[10%] w-[35%] grid grid-col-1 p-1  ">
                     <div className="p-2 border-1 rounded-2xl  mr-[25%] bg-sky-500/[.08] shadow-lg h-[95%] ">
-                        <h1 className="text-xl text-gray-800">Get Started</h1>
+                        <h1 className="text-xl text-gray-800"><b>Schedule New Interview</b></h1>
                         <p className="text-gray-500">
                             Join us in shaping the future of your career. Let's schedule your next interview and take the next step towards finding your perfect fit.</p>
                         <button className="bg-green-500/[.8] text-white p-1 ml-[80%] mb-[1.5%] rounded-md my-[3%]">Schedule</button>
@@ -23,10 +23,18 @@ export default function Dashboard() {
                     </div>
 
                     <div className="p-2 border-1 rounded-2xl bg-sky-500/[.08] ml-[20%] shadow-lg h-[95%] ">
-                        <h1 className="text-xl text-gray-800">Today's Interview</h1>
+                        <h1 className="text-xl text-gray-800"><b>Today's Interview</b></h1>
                         <p className="text-gray-500">
                             Today's packed with potential! Join us as we pave the way for great matches in talent and opportunity.</p>
                         <button className="bg-green-500/[.8] text-white  ml-[85%] p-[1%] rounded-md my-[2%]">Arrivals</button>
+
+                    </div>
+
+                    <div className="p-2 border-1 rounded-2xl  mr-[25%] bg-sky-500/[.08] shadow-lg h-[95%] ">
+                        <h1 className="text-xl text-gray-800"><b>Start An Instant Meet</b></h1>
+                        <p className="text-gray-500">
+                            Meet your match: where hiring great candidates meets great goods.</p>
+                        <button className="bg-green-500/[.8] text-white p-1 ml-[80%] mb-[1.5%] rounded-md my-[3%]">Create</button>
 
                     </div>
                 </div>
